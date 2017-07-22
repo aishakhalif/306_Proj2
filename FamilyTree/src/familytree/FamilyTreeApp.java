3307233c487fa3baf71e0fb3dc0ead53c1e32bb8
aishakhalif <aishakhalif92@gmail.com>
2017-07-22 20:42:42 +0400

    Committing 306_Proj2 files

---
+++ FamilyTree/src/familytree/FamilyTreeApp.java
@@ -0,0 +1,22 @@
+/***********************************************
+ *  Aisha S. Khalif
+ * 32381361
+ * ICT 306
+ ***********************************************/
+
+package familytree;
+
+public class FamilyTreeApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MenuGUI myGUI = new MenuGUI();
+        myGUI.setVisible(true);
+        
+    }
+    
+}
